b6491a9a07332f973f302de28043d82735ca3172
cjlaserna <49669196+cjlaserna@users.noreply.github.com>
2022-06-01 15:49:05 -0700

    Added additional portfolio pages and set up the routing on the init py file

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,18 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+@app.route('/kristen')
+def kristen():
+    return render_template('kristen.html', title="MLH Fellow - Kristen", url=os.getenv("URL"))
+
+@app.route('/helen')
+def helen():
+    return render_template('helen.html', title="MLH Fellow - Helen", url=os.getenv("URL"))
+
+@app.route('/catherine')
+def catherine():
+    return render_template('catherine.html', title="MLH Fellow - Catherine", url=os.getenv("URL"))
